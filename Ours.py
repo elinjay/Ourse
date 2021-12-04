c40ec29ed78b3150bd0831b06d0047f1d9a61333
elinjay <94059679+elinjay@users.noreply.github.com>
2021-12-04 12:02:18 +0100

    Update Ours.py

--- Ours.py
+++ Ours.py
@@ -3,29 +3,28 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
-QQ
-
-## Validation
+## Matrice temps
 Pyr = pd.DataFrame(np.linspace(1996,2021,26)).T
 
+## Validation
+Pyr[2011] = 22
+Pyr[2013] = 25
+Pyr[2014] = 30
+Pyrorien[2015] = 31 dans le noyau oriental (3 m)
+Pyroccid[2015] = 2 m
+Pyr[2016] = 39
+Pyr[2021] = 70 (les 2 versants pyrénéens, principalement dans les Pyrénées centrales)
+
+## Données de réintriduction et d'abatage
 Pyrcentr[1996:1997] = +2 f +1 m +5 b (à Melles)
 Pyr[2004] = -1 dernier de souche
 Pyr[2006] = +2 f (à Haute-Garonne)
 Pyr[2006] = +1 f (à Hautes-Pyrénées)
 Pyr[2006] = +1 m +1 f (à Arbas)
-Pyr[2011] =  22
-Pyr[2013] =  25
 Pyr[2014] = -1
-Pyr[2014] = 30
-Pyrorien[2014] = 31 dans le noyau oriental (3 m)
-Pyroccid[2014] = 2 m
 Pyr[2016] = +1 m
-Pyr[2016] = 39
 Pyr[2016] = - 3
 Pyroccid[2018] = +2 f
 Pyr[2020] = -1 m
 Pyr[2020] = -1 m (à Ariège)
 Pyr[2020] = -1 f (la comarca de Ribagorza en région Aragón)
-Pyr[2021] = 70 (les 2 versants pyrénéens, principalement dans les Pyrénées centrales)
